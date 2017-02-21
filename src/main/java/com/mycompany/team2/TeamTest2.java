0fccecca9ad84c77d928d456f4cd25bf895e7cf6
David <David@10.15.208.176>
2017-02-21 22:48:42 +0000

    16 iteration of Pom

--- src/main/java/com/mycompany/team2/TeamTest2.java
+++ src/main/java/com/mycompany/team2/TeamTest2.java
@@ -27,6 +27,7 @@ public class TeamTest2 {
         System.out.println("bbububub");
         System.out.println("Emailtest4");
          System.out.println("Emailtestfddf");
+         System.out.println("Yoouj");
         
         
     }
